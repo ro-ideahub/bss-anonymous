a277d2810d23e8520eec7b12f1814aa49502f531
ro-ideahub <ideahub@brettrm001.com>
2024-06-06 16:54:32 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,20 @@
 while wait(0.1) do
     if game:IsLoaded() then
+        local hiveClaimed = false
         for i, v in pairs(game.Workspace.Honeycombs:GetChildren()) do
             if v.Owner.Value == game.Players.LocalPlayer then
                 v.Display.Gui.Frame.OwnerName.Text = "Onett"
+                hiveClaimed = true
             end
         end
         for i, v in pairs(game.Workspace.HivePlatforms:GetChildren()) do
             if v.PlayerRef.Value == game.Players.LocalPlayer then
                 v.Circle.SurfaceGui.TextLabel.Text = "Onett"
+                hiveClaimed = true
             end
         end
-        break
+        if hiveClaimed then
+            break
+        end
     end
 end
