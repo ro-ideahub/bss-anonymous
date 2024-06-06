5bbeb9e08969394d9cf1ab699b7cdbe0f973a6ed
ro-ideahub <ideahub@brettrm001.com>
2024-06-06 16:40:25 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,5 +6,10 @@ while wait(0.1) do
                 break
             end
         end
+        for i, v in pairs(game.Workspace.HivePlatforms:GetChildren()) do
+            if v.PlayerRef == game.Players.LocalPlayer then
+                v.Circle.SurfaceGui.TextLabel.Text = "Onett"
+            end
+        end
     end
 end
